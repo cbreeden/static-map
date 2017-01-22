ed41dd77d5043cfcd3869763563ed3b733a61678
Christopher Breeden <github@u.breeden.cc>
2017-01-22 14:00:07 -0600

    working again

--- build/build.rs
+++ build/build.rs
@@ -22,7 +22,7 @@ use staticmap_builder::Builder;
 use staticmap_hashers::fxhash;
 
 fn main() {
-    make_phfset();
+    // make_phfset();
     // make_fnvstatic();
     make_fxstatic();
     // make_fxinline();
@@ -32,38 +32,35 @@ macro_rules! display {
     ($id:expr) => (format!("{}", $id))
 }
 
-fn make_phfset() {
-    let output = Path::new(&env::var_os("OUT_DIR").expect("OUT_DIR")).join("phf.rs");
-    let mut file = BufWriter::new(File::create(&output).expect("phf.rs file"));
+// fn make_phfset() {
+//     let output = Path::new(&env::var_os("OUT_DIR").expect("OUT_DIR")).join("phf.rs");
+//     let mut file = BufWriter::new(File::create(&output).expect("phf.rs file"));
 
-    let mut map = phf_codegen::Map::new();
-    for &(code, glyph) in GLYPHS.iter() {
-        map.entry(code, &display!(glyph));
-    }
+//     let mut map = phf_codegen::Map::new();
+//     for &(code, glyph) in GLYPHS.iter() {
+//         map.entry(code, &display!(glyph));
+//     }
 
-    write!(&mut file, "pub static PHF_MAP: phf::Map<u32, Glyph> = ").unwrap();
-    map.build(&mut file).unwrap();
-    write!(&mut file, ";\n").unwrap();
-}
+//     write!(&mut file, "pub static PHF_MAP: phf::Map<u32, Glyph> = ").unwrap();
+//     map.build(&mut file).unwrap();
+//     write!(&mut file, ";\n").unwrap();
+// }
 
 fn make_fxstatic() {
     let output = Path::new(&env::var_os("OUT_DIR").expect("OUT_DIR")).join("fx.rs");
     let mut file = BufWriter::new(File::create(&output).expect("fx.rs file"));
 
-    let mut g: Vec<Glyph> = Vec::new();
     let mut t = Builder::with_capacity(GLYPHS.len(), fxhash::FxHashBuilder::default());
 
-    for &(code, glyph) in GLYPHS.iter() {
-        t.insert(code, display!(g.len()));
-        g.push(glyph);
+    for (idx, &(code, _)) in GLYPHS.iter().enumerate() {
+        t.insert(code, idx);
     }
 
     write!(&mut file, "static FX_MAP: Map<u32, usize, fxhash::FxHashBuilder> = ").unwrap();
     t.build(&mut file).unwrap();
 
-    write!(&mut file, "static FX_GLYPHS: [Glyph; {}] = [", g.len()).unwrap();
-
-    for glyph in g {
+    write!(&mut file, "static FX_GLYPHS: [Glyph; {}] = [", GLYPHS.len()).unwrap();
+    for &(_, glyph) in GLYPHS.iter() {
         write!(&mut file, "{}, ", glyph).unwrap();
     }
 
--- staticmap-builder/src/lib.rs
+++ staticmap-builder/src/lib.rs
@@ -1,5 +1,6 @@
 use std::cmp;
 use std::mem;
+use std::default::Default;
 use std::fmt;
 use std::fmt::Display;
 use std::fmt::Debug;
@@ -10,29 +11,22 @@ use std::io;
 
 const MIN_TABLE_SIZE: usize = 32;
 
-#[derive(Default, Debug)]
-pub struct Entry<K> {
-  pub key:   K,
-  pub value: String,
-}
-
-pub struct Builder<K, S> {
+pub struct Builder<K, V, S> {
   pub hashes:  Vec<usize>,
-  pub entries: Vec<Entry<K>>,
+  pub entries: Vec<(K, V)>,
   pub hasher:  S,
 }
 
-impl<K, S> Builder<K, S>
-    where K: Hash + Eq + Default + Display,
+impl<K, V, S> Builder<K, V, S>
+    where K: Hash + Eq + Default + Debug,
+          V: Default + Debug,
           S: BuildHasher + Debug {
-  pub fn with_capacity(size: usize, hasher: S) -> Builder<K, S> {
+  pub fn with_capacity(size: usize, hasher: S) -> Builder<K, V, S> {
     // Builder size must be a power of two.
     let cap = cmp::max((size * 10/9).next_power_of_two(), MIN_TABLE_SIZE);
     let mut entries = Vec::with_capacity(cap);
 
-    for _ in 0..cap {
-      entries.push(Entry::<K>::default());
-    }
+    for _ in 0..cap { entries.push(<(K,V) as Default>::default()); }
 
     Builder {
       hashes:  vec![0; cap],
@@ -41,16 +35,13 @@ impl<K, S> Builder<K, S>
     }
   }
 
-  pub fn insert(&mut self, key: K, value: String) -> usize {
+  pub fn insert(&mut self, key: K, value: V) -> usize {
     let mask = self.entries.len() - 1;
     let mut hash = self.hash(&key);
     let mut pos  = hash & mask;
     let mut dist = 0;
 
-    let mut entry = Entry {
-      key:   key,
-      value: value,
-    };
+    let mut entry = (key, value);
 
     loop {
       let probe_hash = unsafe { self.hashes.get_unchecked_mut(pos) };
@@ -92,7 +83,7 @@ impl<K, S> Builder<K, S>
     write!(f, "  ],\n  entries: &[  \n")?;
 
     for entry in self.entries.iter() {
-        write!(f, "{}, ", entry)?;
+        write!(f, "{:?}, ", entry)?;
     }
 
     write!(f, "  ],\n")?;
@@ -105,12 +96,5 @@ impl<K, S> Builder<K, S>
     key.hash(&mut hasher);
     let hash =  hasher.finish() as usize;
     if hash == 0 { 1 } else { hash }
-  }
-}
-
-impl<K> fmt::Display for Entry<K>
-    where K: fmt::Display {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "({}, {})", self.key, self.value)
   }
 }
