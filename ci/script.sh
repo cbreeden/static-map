929431f60d126a2bb2a08293e8d1dd1d2687d1c4
Christopher Breeden <github@u.breeden.cc>
2017-07-29 12:56:40 -0500

    trigger ci

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross build --target $TARGET -p $CRATE_NAME
+    cross build --target $TARGET --release -p $CRATE_NAME
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cross test --target $TARGET -p $CRATE_NAME
+    cross test --target $TARGET --release -p $CRATE_NAME
 }
 
 # we don't run the "test phase" when doing deploys
